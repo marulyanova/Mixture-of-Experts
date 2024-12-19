1881c4350ddd3e65d3eacb9ba9e95ad49206c5ec
anyhhope <hopeanysimova@yandex.ru>
2024-12-19 17:28:36 +0300

    add loading dataset params

--- README.md
+++ README.md
@@ -4,6 +4,11 @@
 
 # Запуск эскпериментов
 
+## 1. Запуск формирования датасета
+```bash
+python3 dataset/dataset.py  --config-name train_dataset_params.yaml
+```
+
 ## 2. Запуск обучения
 ```bash
 python3 train/train.py  --config-name model_params.yaml
--- dataset/dataset.py
+++ dataset/dataset.py
@@ -1,13 +1,27 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(__file__, "../../")))
+
+import click
+from pathlib import Path
 from datasets import Dataset
 from transformers import BertTokenizerFast
 import pandas as pd
 
-# Задаем токенизатор
-tokenizer = BertTokenizerFast.from_pretrained("bert-base-uncased")
+from config_utils.load_config import load_params_from_yaml, DatasetParamsSchema
 
-# Загружаем данные
-data_path = "dataset/data/raw/data.csv"
-df = pd.read_csv(data_path)
 
-# Преобразуем в формат Hugging Face Dataset
-dataset = Dataset.from_pandas(df[["title", "body", "subreddit"]])
+@click.command()
+@click.option('--config-name', type=Path, required=True)
+def main(config_name):
+    data_params = load_params_from_yaml(config_name, DatasetParamsSchema)
+
+    tokenizer = BertTokenizerFast.from_pretrained(data_params.tokenizer_name)
+    df = pd.read_csv(data_params.data_path)
+
+    dataset = Dataset.from_pandas(df[["title", "body", "subreddit"]])
+    print(dataset[0])
+    return 
+
+if __name__=="__main__":
+    main()
