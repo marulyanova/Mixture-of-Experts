526aabb3c4b0623de745bcdc6de63d7510f6e39f
anyhhope <hopeanysimova@yandex.ru>
2024-12-20 12:03:28 +0300

    add loading train params frpm yaml

---
+++ config_utils/train_params.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+@dataclass
+class TrainParams:
+    batch_size: int
+    n_epochs: int
+    learning_rate: float
+    gradient_accumulation_steps: int
+    experiment_name: str
+    weight_decay: float
+    warmup_proportion: float
+    tokenizer_mask_id: int #TODO move to data params
+    eval_steps: int
+    save_steps: int
+    save_path: str
+    random_seed: int
